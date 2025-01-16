6c8a49679adf16e0aa48c1b0a92510eb427b29ba
Alberts Zemzale <alberts.zemzale@gmail.com>
2025-01-16 09:18:21 +0200

    Add support for turso

--- src/server/db.ts
+++ src/server/db.ts
@@ -1,15 +1,20 @@
 import { PrismaClient } from "@prisma/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
+import { createClient } from "@libsql/client";
+
 
 import { env } from "~/env";
 
-const createPrismaClient = () =>
-  new PrismaClient({
-    log:
-      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-  });
+const libsql = createClient({
+    url: env.DATABASE_URL,
+    authToken: env.TURSO_TOKEN,
+});
+
+const adapter = new PrismaLibSQL(libsql);
+const createPrismaClient = () => new PrismaClient({ adapter });
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: ReturnType<typeof createPrismaClient> | undefined;
+    prisma: ReturnType<typeof createPrismaClient> | undefined;
 };
 
 export const db = globalForPrisma.prisma ?? createPrismaClient();
